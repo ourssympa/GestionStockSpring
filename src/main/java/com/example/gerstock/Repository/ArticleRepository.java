2e02db707f63a4d3a61d65f1d97f0c7f7d9810a4
ourssympa <gracienfiomedon@gmail.com>
2022-04-22 11:52:13 +0200

    first

---
+++ src/main/java/com/example/gerstock/Repository/ArticleRepository.java
@@ -0,0 +1,9 @@
+package com.example.gerstock.Repository;
+
+import com.example.gerstock.Model.Article;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ArticleRepository extends JpaRepository<Article,Integer>{
+}
---
+++ src/main/java/com/example/gerstock/Service/ArticleService.java
@@ -0,0 +1,28 @@
+package com.example.gerstock.Service;
+
+import com.example.gerstock.Model.Article;
+import com.example.gerstock.Repository.ArticleRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class ArticleService {
+    @Autowired
+    ArticleRepository articleRepository;
+    public void saveArticle(Article article){
+        articleRepository.save(article);
+    }
+
+    public List<Article> listeArticle(){
+        return articleRepository.findAll();
+    }
+
+    public Article oneArticle(int id){
+        return articleRepository.findById(id).get();
+    }
+    public void deleteArticle(int id){
+        articleRepository.deleteById(id);
+    }
+}
